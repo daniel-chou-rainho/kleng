aef8590de5a60bcb19f5bde6b39ccb4407ddb49c
CR-Daniel <choudaniel3@gmail.com>
2024-03-29 12:41:18 +0100

    theme toggle

--- script.js
+++
@@ -1,7 +0,0 @@
-var editor = CodeMirror(document.getElementById("editor"), {
-    mode:  "javascript", // Set the mode according to your needs, e.g., HTML, CSS, JS
-    theme: "monokai",    // Set your preferred theme
-    lineNumbers: false
-});
-
-editor.setSize("100%", "100%");
